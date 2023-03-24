6adc83f8c4fbbae9f08142eb1479ae71f1ebdaba
Candice Bimont <cbimont@simplicite.fr>
2023-03-24 17:06:07 +0100

    Comments

--- src/com/simplicite/objects/ProjectManagement/PmAssignment.java
+++ src/com/simplicite/objects/ProjectManagement/PmAssignment.java
@@ -12,30 +12,30 @@ import com.simplicite.util.tools.*;
 public class PmAssignment extends ObjectDB {
 	private static final long serialVersionUID = 1L;
 	@Override
-	public String preDelete() {
+	public String preDelete() {// if we deleted assignment we have to update the number of task of user
 		ObjectDB prdTask = this.getGrant().getTmpObject("PmTask");
 		synchronized(prdTask){
 			// select = chargement dans l'instance des valeurs en base à partir d'une clef technique (id)
             prdTask.select(this.getFieldValue("pmAssPmTaskid"));
-			if(prdTask.getStatus().equals("TODO") ||prdTask.getStatus().equals("DOING")){
+			if(prdTask.getStatus().equals("TODO") ||prdTask.getStatus().equals("DOING")){// if the task assigned is in to do or doing status we decrease the number of task
 				decreaseUserNbTask();
 			}
 		}
 		return super.preDelete();
 	}
 	@Override
-	public String postCreate() {
+	public String postCreate() {// if we created assignment we have to update the number of task of user
 		ObjectDB prdTask = this.getGrant().getTmpObject("PmTask");
 		synchronized(prdTask){
 			// select = chargement dans l'instance des valeurs en base à partir d'une clef technique (id)
             prdTask.select(this.getFieldValue("pmAssPmTaskid"));
-			if(prdTask.getStatus().equals("TODO") ||prdTask.getStatus().equals("DOING")){
+			if(prdTask.getStatus().equals("TODO") ||prdTask.getStatus().equals("DOING")){// if the task assigned is in to do or doing status we increase the number of task
 				increaseUserNbTask();
 			}
 		}
 		return super.postCreate();
 	}
-	public void increaseUserNbTask(){
+	public void increaseUserNbTask(){// invoke the increase methode of object user
 		ObjectDB prd = this.getGrant().getTmpObject("PmUser");
         synchronized(prd){
             // select = chargement dans l'instance des valeurs en base à partir d'une clef technique (id)
@@ -50,7 +50,7 @@ public class PmAssignment extends ObjectDB {
             prd.save();
         }
 	}
-	public void decreaseUserNbTask(){
+	public void decreaseUserNbTask(){// invoke the decrease methode of object user
 		ObjectDB prd = this.getGrant().getTmpObject("PmUser");
         synchronized(prd){
             // select = chargement dans l'instance des valeurs en base à partir d'une clef technique (id)
@@ -59,7 +59,7 @@ public class PmAssignment extends ObjectDB {
 			try {
 				prd.invokeMethod("decreaseNbTask",null, null);
 			} catch (Exception e) {
-				AppLog.info(" DEBUG "+"PmAssignment "+"increaseUserNbTask "+"invokeMethod exception: "+e, getGrant());
+				AppLog.error(getClass(),"decreaseUserNbTask","invokeMethod exception",e, getGrant());
 			}
 			
             prd.save();
