495522b9b78680738cf2c1839f80cd76ade24cf0
Candice Bimont <cbimont@simplicite.fr>
2023-03-27 12:30:47 +0200

    pubHtml publication: table task

--- src/com/simplicite/objects/ProjectManagement/PmProject.java
+++ src/com/simplicite/objects/ProjectManagement/PmProject.java
@@ -1,5 +1,7 @@
 package com.simplicite.objects.ProjectManagement;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 import com.simplicite.util.*;
@@ -12,14 +14,38 @@ import com.simplicite.util.tools.*;
 public class PmProject extends ObjectDB {
 	private static final long serialVersionUID = 1L;
 	public String pubHtml(){
-		String html="<h1>"+getFieldValue("pmPrjName")+"</h1>"+"\n";
-
+		LocalDate dateObj = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String now = dateObj.format(formatter);
+		String html="<head><title>"+getFieldValue("pmPrjName")+": "+now+"</title></head>"+"\n";
+		html+="<h1>"+getFieldValue("pmPrjName")+" "+now+"</h1>"+"\n";
 		String sqlQuery = "select row_id, pm_vrs_version, pm_vrs_date_publication from pm_version where pm_vrs_status='PUBLISHED' AND pm_vrs_prj_id="+getRowId()+"order by pm_vrs_date_publication DESC";
-		html+= "<ul>"+"\n";
+		
 		for(String[] row : getGrant().query(sqlQuery)){
-			html += "<li>"+row[1]+": "+row[2]+"</li>\n";
+			html += "<h1>"+row[1]+"</h1>\n";
+			html += "Published on "+row[2]+"\n";
+			String sqlQueryTask = "select row_id, pm_tsk_number, pm_tsk_title,pm_tsk_description,pm_tsk_status,pm_tsk_priority,pm_tsk_effective_closing_date,pm_tsk_expected_duration  from pm_task where pm_tsk_vrs_id="+row[0]+"order by pm_tsk_creation DESC";
+			int id = 0; int number = 1; int title= 2; int description= 3; int status= 4; int priority= 5; int effectiveClosingDate= 6; int expectedDuration=7;
+			html += "<table>"+"\n";
+			html += "<thead><tr><th>Number</th><th>Title</th><th>Description</th><th>Status</th><th>Priority</th><th>Effective closing date</th><th>Expeted Duration</th></tr></thead>"+"\n";
+			html += "<tbody>"+"\n";
+			for(String[] rowTask : getGrant().query(sqlQueryTask)){
+				html += "<tr>"+"\n";
+				html += "<td>"+rowTask[number]+"</td>"+"\n";
+				html += "<td>"+rowTask[title]+"</td>"+"\n";
+				html += "<td>"+rowTask[description]+"</td>"+"\n";
+				html += "<td>"+rowTask[status]+"</td>"+"\n";
+				html += "<td>"+rowTask[priority]+"</td>"+"\n";
+				html += "<td>"+rowTask[effectiveClosingDate]+"</td>"+"\n";
+				html += "<td>"+rowTask[expectedDuration]+"</td>"+"\n";
+				html += "</tr>"+"\n";
+			}
+			html += "</tbody>"+"\n";
+			html += "</table>"+"\n";
+
+			
 		}
-		html+= "</ul>"+"\n";
+		
 		return html;
 	}
 	
