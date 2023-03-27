e054c5ce7bc0cef03957ec3c03416ad4ebbea3ab
Candice Bimont <cbimont@simplicite.fr>
2023-03-27 14:41:17 +0200

    pubHtml css resource

--- src/com/simplicite/objects/ProjectManagement/PmProject.java
+++ src/com/simplicite/objects/ProjectManagement/PmProject.java
@@ -17,7 +17,7 @@ public class PmProject extends ObjectDB {
 		LocalDate dateObj = LocalDate.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 		String now = dateObj.format(formatter);
-		AppLog.info(getResource("pmPrjPubHtmlStyle").getCode(), getGrant())
+		AppLog.info(getResource("pmPrjPubHtmlStyle").getCode(), getGrant());
 		String html="<head><title>"+getFieldValue("pmPrjName")+": "+now+"</title><style type='text/css'></style></head>"+"\n";
 		html+="<h1>"+getFieldValue("pmPrjName")+" "+now+"</h1>"+"\n";
 		String sqlQuery = "select row_id, pm_vrs_version, pm_vrs_date_publication from pm_version where pm_vrs_status='PUBLISHED' AND pm_vrs_prj_id="+getRowId()+"order by pm_vrs_date_publication DESC";
