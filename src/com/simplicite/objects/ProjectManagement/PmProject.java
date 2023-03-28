3adc44a6f536edee5dedfe645ebe44a348ffba25
Candice Bimont <cbimont@simplicite.fr>
2023-03-28 16:59:42 +0200

    label front

--- src/com/simplicite/objects/ProjectManagement/PmProject.java
+++ src/com/simplicite/objects/ProjectManagement/PmProject.java
@@ -64,6 +64,7 @@ public class PmProject extends ObjectDB {
 						}
 						String labelString=labelArray.toString();
 						if(labelString.length()>0){taskJson.put("PmLabel",labelString.substring(0,labelString.length()-2));}
+						
 						taskArray.put(taskJson);
 
 					}
