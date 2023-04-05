8afe3f9ff77d7d13e624e08e359fb5124ffe4b99
Candice Bimont <cbimont@simplicite.fr>
2023-04-05 18:15:20 +0200

    tesk deletion debug

--- src/com/simplicite/objects/ProjectManagement/PmVersion.java
+++ src/com/simplicite/objects/ProjectManagement/PmVersion.java
@@ -91,8 +91,6 @@ public class PmVersion extends ObjectDB {
 	*/ 
 	public String deferTask(){
 		String msg = new String();
-		List<String> taskSelected= getSelectedIds();
-		AppLog.info("DEBUG "+getAction("PM_DEFER_TASK").getType()+" "+taskSelected, getGrant());
 		String[] selected= getAction("PM_DEFER_TASK").getConfirmField(getGrant().getLang(), "pmDtVrsVersion").getValue().split(":");
 		if(!selected[0].equals("PmVersion")){
 			msg= Message.formatError("PM_ERR_DEFER_TASK_OBJECT_TYPE", null, "pmVrsStatus");
