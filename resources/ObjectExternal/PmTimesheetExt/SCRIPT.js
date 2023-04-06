42332bf4ad0937ac8060796c896baf84018ffb17
Candice Bimont <cbimont@simplicite.fr>
2023-04-06 11:47:04 +0200

    PmTimesheetExt

--- resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
+++ resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
@@ -1,7 +1,7 @@
 var PmTimesheetExt = (function() {
 	//CRA
     function render(params) {
-        console.log("DEBUG");
+        $ui.log("DEBUG");
         $ui.displayTimesheet(null, "PmUser", $ui.grant.getUserID(), "PmTimeSheetAssign");
     }
     return { render: render };
