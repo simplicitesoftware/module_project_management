d6a562e3e2ec25ac7d99345e63c9f30ada0cc150
Candice Bimont <cbimont@simplicite.fr>
2023-04-06 17:43:34 +0200

    PmTimesheet calcul consumed

--- src/com/simplicite/objects/ProjectManagement/PmAssignmentPmTimeSheetAssign.java
+++ src/com/simplicite/objects/ProjectManagement/PmAssignmentPmTimeSheetAssign.java
@@ -8,6 +8,8 @@ import org.json.JSONObject;
 import com.simplicite.util.*;
 import com.simplicite.util.Timesheet.*;
 import com.simplicite.util.exceptions.GetException;
+import com.simplicite.util.exceptions.SaveException;
+import com.simplicite.util.exceptions.ValidateException;
 import com.simplicite.util.tools.*;
 
 /**
@@ -17,7 +19,8 @@ public class PmAssignmentPmTimeSheetAssign extends ObjectDB {
 	private static final long serialVersionUID = 1L;
 	@Override
 	public List<String> postSaveTimesheet(Timesheet ts) {
-		Grant g = getGrant();
+		Grant g = getGrant(); 
+		List<String> msgs= new ArrayList<>();
         List<TimesheetLine> lines = ts.getCurrentLines();
 		try {
 			if (ts.getObjectResource1().getName().equals("PmUser")){
@@ -25,39 +28,30 @@ public class PmAssignmentPmTimeSheetAssign extends ObjectDB {
 					ObjectDB tmpAss = g.getTmpObject("PmAssignment");
 					BusinessObjectTool ot = tmpAss.getTool();
 					AppLog.info("Debug: "+tsLine.getAssignRowId(), g);
+					if (ot.getForCreateOrUpdate(
+		                    new JSONObject()
+		                            .put("row_id", tsLine.getAssignRowId())
+		                            )) {
+										String conso = g.simpleQuery("select sum(tsh_total1) from pm_assignment_pm_time_sheet_assign where tsh_parent_id="+tsLine.getAssignRowId());
+										tmpAss.setFieldValue("pmAssConsumed",conso);
+			            				ot.validateAndSave();
+									}
 				}
 			}
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			msgs.add(Message.formatError("PM_ERR_EXPTION_TS", null, null));
+			AppLog.error("postSaveTimesheet", e, g);
+		} catch (GetException e) {
+			msgs.add(Message.formatError("PM_ERR_EXPTION_TS", null, null));
+			AppLog.error("postSaveTimesheet", e, g);
+		} catch (ValidateException e) {
+			msgs.add(Message.formatError("PM_ERR_EXPTION_TS", null, null));
+			AppLog.error("postSaveTimesheet", e, g);
+		} catch (SaveException e) {
+			msgs.add(Message.formatError("PM_ERR_EXPTION_TS", null, null));
+			AppLog.error("postSaveTimesheet", e, g);
 		}
 		return super.postSaveTimesheet(ts);
 	}
 
 }
-/* List<TimesheetLine> h = ts.getCurrentLines();
-			if (ts.getObjectResource1().getName().equals("CraEmploye")){
-				for (TimesheetLine line : h)
-				{
-					String objname = "CraCompteCraEmploye";
-	        		boolean[] oldcrud = g.changeAccess(objname,true,true,true,false);
-	            	ObjectDB o = g.getTmpObject(objname);
-	            	o.resetFilters();
-		            o.resetValues();
-		            BusinessObjectTool ot = o.getTool();
-		            if (ot.getForCreateOrUpdate(
-		                    new JSONObject()
-		                            .put("craAccountempcraCptAId", line.getResource2RowId())
-		                            .put("craEmployeId", line.getResource1RowId())
-		                            .put("craAppcracomptecraemployeDebut", "='"+line.getBeginKey()+"'")
-		                            .put("craAppcracomptecraemployeFin", line.getEndKey()!=null?"='"+line.getEndKey()+"'":"is null")
-		                            )) {
-			            //double c = Tool.parseDouble(line.getTotals()[0]);
-			            String conso = g.simpleQuery("select sum(tsh_total1) from cra_compte_cra_employe_imputation where tsh_parent_id="+line.getAssignRowId());
-			            AppLog.debug("postSaveTimesheet**" + conso, g);
-			            o.setFieldValue("craAppcracomptecraemployeConso",conso);
-			            ot.validateAndSave();
-		             }
-			         g.changeAccess(objname,oldcrud);
-	            }
-			} */
