6cf4b680fb797b669d962f9ccb5c72daf41f21df
Candice Bimont <cbimont@simplicite.fr>
2023-04-07 15:50:59 +0200

    test ext

--- resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
+++ resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
@@ -1,7 +1,26 @@
 var PmTimesheetExt = (function() {
 	//CRA
     function render(params) {
-        $ui.displayTimesheet($('#ts'), "PmUser", $ui.grant.getUserID(), "PmTimeSheetAssign");
+        var userid =$ui.grant.getUserID();
+        $ui.displayTimesheet($('#ts'), "PmUser",userid , "PmTimeSheetAssign");
+        try {
+			if (typeof Mustache === 'undefined') throw 'Mustache not available';
+			var div = $('#recap');
+			app = $ui.getAjax();
+	
+			var affect = app.getBusinessObject('PmAssignment'),
+                pmAssPmUserid = userid;
+				affect.search(function(rows) {
+					div.html(affect);
+				}, {
+					pmAssPmUserid: pmAssPmUserid
+				}, {error: function(err) {console.error(err);}}
+					
+			);
+			
+		} catch(e) {
+			console.error('Render error: ' + e.message);
+		}
     }
     return { render: render };
 })();
