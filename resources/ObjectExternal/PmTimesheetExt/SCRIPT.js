46f537cbc17d1fd690bc09a1ed1d49b1b0dfc260
Candice Bimont <cbimont@simplicite.fr>
2023-04-11 17:43:58 +0200

    test ext

--- resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
+++ resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
@@ -4,14 +4,7 @@ var PmTimesheetExt = (function($) {
     function render(params) {
         var userid =$ui.grant.getUserID();
         $ui.displayTimesheet($('#ts'), "PmUser",userid , "PmTimeSheetAssign");
-        var app = $ui.getAjax(); 
-		var extobj = "PmTimesheetRecapExt"; 
-		var embedded = true; 
-		var param = null; 
-		var url = app.getExternalObjectURL(extobj, param, embedded);
-		console.log("url = " + url);
-		ctn = $('#recap'); 
-		$ui.loadURL(ctn, url, { showNav:false });
+        
     }        
     return { render: render };
 })(jQuery);
