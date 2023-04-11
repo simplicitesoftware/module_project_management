40d593860777c25cc26e8804c27ed63ecb6ddd75
Candice Bimont <cbimont@simplicite.fr>
2023-04-11 11:13:17 +0200

    test ext

--- resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
+++ resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
@@ -3,23 +3,28 @@ var PmTimesheetExt = (function($) {
     function render(params) {
         var userid =$ui.grant.getUserID();
         $ui.displayTimesheet($('#ts'), "PmUser",userid , "PmTimeSheetAssign");
-		var div=$('#recap')
-        var template =$('#pm-ass-template').html();
-		if (userid) {
-            // Search the client
-			app = $ui.getAjax();
-			var affect = app.getBusinessObject('PmAssignment');
-            affect.search(function(list) {
-                //div.html(Mustache.render(template,list))
-                if (list && list.length) {
-                    list.forEach(ass => div.html(div.html()+formatLineAss(ass)));
-                }
-            }, {
-                pmAssPmUserid: userid
-            });
-        }
-        else $ui.alert("No client id");
-    }
+        try {
+			if (typeof Mustache === 'undefined') throw 'Mustache not available';
+            var div=$('#recap')
+            var template =$('#pm-ass-template').html();
+            if (userid) {
+                // Search the client
+                app = $ui.getAjax();
+                var affect = app.getBusinessObject('PmAssignment');
+                affect.search(function(list) {
+                    //div.html(Mustache.render(template,list))
+                    if (list && list.length) {
+                        list.forEach(ass => div.html(div.html()+formatLineAss(ass)));
+                    }
+                }, {
+                    pmAssPmUserid: userid
+                });
+            }
+            else $ui.alert("No client id");
+        } catch(e) {
+			console.error('Render error: ' + e.message);
+		}
+    }        
     return { render: render };
 })(jQuery);
 function formatLineAss(ass) {
