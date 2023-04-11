9040c277aa0dd7f3d158665989ef973fb20c7f5e
Candice Bimont <cbimont@simplicite.fr>
2023-04-11 10:27:18 +0200

    test ext

--- resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
+++ resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
@@ -1,28 +1,33 @@
 var PmTimesheetExt = (function($) {
 	//CRA
     function render(params) {
-        try {
-			if (typeof Mustache === 'undefined') throw 'Mustache not available';
-			var userid =$ui.grant.getUserID();
+            var userid =$ui.grant.getUserID();
             $ui.displayTimesheet($('#ts'), "PmUser",userid , "PmTimeSheetAssign");
-            var div=$('#recap')
-            var template =div.html();
-            if (userid) {
-                // Search the client
-                app = $ui.getAjax();
-                var affect = app.getBusinessObject('PmAssignment');
-                affect.search(function(list) {
-                    div.html(Mustache.render(template,list));
-                }, {
-                    pmAssPmUserid: userid
-                });
-            }
-            else $ui.alert("No user id");
+        try {
+           
+			if (typeof Mustache === 'undefined') throw 'Mustache not available';
 		} catch(e) {
 			console.error('Render error: ' + e.message);
 		}
 
 
+
+
+
+        
+		var div=$('#recap')
+        var template =div.html();
+		if (userid) {
+            // Search the client
+			app = $ui.getAjax();
+			var affect = app.getBusinessObject('PmAssignment');
+            affect.search(function(list) {
+                div.html(Mustache.render(template,list))
+            }, {
+                pmAssPmUserid: userid
+            });
+        }
+        else $ui.alert("No user id");
     }
     return { render: render };
 })();
