8749ce408aeba51ef0201428bfe126ff2809e115
Candice Bimont <cbimont@simplicite.fr>
2023-04-11 17:10:54 +0200

    test ext

--- resources/ObjectExternal/PmTimesheetRecapExt/SCRIPT.js
+++ resources/ObjectExternal/PmTimesheetRecapExt/SCRIPT.js
@@ -2,6 +2,7 @@ var PmTimesheetRecapExt = (function($) {
     let app;
 	//CRA
     function render(params) {
+        console.log("DEBUG render PmTimesheetRecapExt ---------------------------------------------------------");
         var userid =$ui.grant.getUserID();
         try {
 			//if (typeof Mustache === 'undefined') throw 'Mustache not available';
