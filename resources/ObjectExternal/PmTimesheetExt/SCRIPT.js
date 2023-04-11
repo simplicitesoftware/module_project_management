cef5b714981e56c2678d71a20d035ae1595ba96f
Candice Bimont <cbimont@simplicite.fr>
2023-04-11 11:24:55 +0200

    test ext

--- resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
+++ resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
@@ -38,6 +38,6 @@ function formatLineAss(ass) {
   }
 
 function formatLineHeader(ass) {
-    return '<div class="table-header"><div class="header__item">'+ass.pmAssRole.display+'</div><div class="header__item small">'+ass.labelQuantity.display+'</div><div class="header__item small">'+ass.labelConsumed.display+'</div><div class="header__item big"></div></div>'
+    return '<div class="table-header"><div class="header__item">'+ass.pmAssRole.label+'</div><div class="header__item small">'+ass.labelQuantity.label+'</div><div class="header__item small">'+ass.labelConsumed.label+'</div><div class="header__item big"></div></div>'
     ;
 }
