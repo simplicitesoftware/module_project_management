09938c9555bd2d3f800425564742d13adb1c194a
Candice Bimont <cbimont@simplicite.fr>
2023-04-11 16:52:09 +0200

    test ext

--- resources/ObjectInternal/PmAssignmentPmTimeSheetAssign/SCRIPT.js
+++ resources/ObjectInternal/PmAssignmentPmTimeSheetAssign/SCRIPT.js
@@ -9,7 +9,15 @@
 			var p = o.locals.ui;
 			if (p && o.isMainInstance()) {
 				p.form.onload = function(ctn, obj, params) {
-					//...
+					var app = $ui.getAjax(); 
+					var extobj = "PmTimesheetRecapExt"; 
+					var embedded = true; 
+					
+					var param = null; 
+					var url = app.getExternalObjectURL(extobj, param, embedded);
+					console.log("url = " + url);
+					ctn = "$('#recap')"; 
+					$ui.loadURL(ctn, url, { showNav:false });
 				};
 			}
 			//...
