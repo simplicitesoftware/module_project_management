a23b94a62372b2f4fafcf2668ff3ed71b6b9534f
Candice Bimont <cbimont@simplicite.fr>
2023-04-19 17:18:13 +0200

    cra recap

--- resources/ObjectInternal/PmAssignmentPmTimeSheetAssign/SCRIPT.js
+++ resources/ObjectInternal/PmAssignmentPmTimeSheetAssign/SCRIPT.js
@@ -1,11 +1,13 @@
 // PmAssignmentPmTimeSheetAssign front side hook
 (function(ui) {
+	console.log("DEBUG fonction(ui) PmAssignmentPmTimeSheetAssign ------------------------------------");
 	if (!ui) return;
 	var app = ui.getAjax();
 	// Hook called by each object instance
 	Simplicite.UI.hooks.PmAssignmentPmTimeSheetAssign = function(o, cbk) {
+		console.log("DEBUG Simplicite.UI.hooks.PmAssignmentPmTimeSheetAssign ------------------------------------");
 		try {
-			
+			console.log("PmAssignmentPmTimeSheetAssign hooks loading...");
 			var p = o.locals.ui;
 			if (p && o.isMainInstance()) {
 				p.form.onload = function(ctn, obj, params) {
@@ -21,6 +23,7 @@
 		} catch (e) {
 			app.error("Error in Simplicite.UI.hooks.PmAssignmentPmTimeSheetAssign: " + e.message);
 		} finally {
+			console.log("PmAssignmentPmTimeSheetAssign hooks loaded.");
 			cbk && cbk(); // final callback
 		}
 	};
