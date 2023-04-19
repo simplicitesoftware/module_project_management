ec7240ca0ef612d8138ab0104bb9a06cd9c32e52
Candice Bimont <cbimont@simplicite.fr>
2023-04-19 17:31:19 +0200

    cra recap

--- resources/ObjectInternal/PmAssignment/SCRIPT.js
+++ resources/ObjectInternal/PmAssignment/SCRIPT.js
@@ -6,18 +6,14 @@
 	Simplicite.UI.hooks.PmAssignment = function(o, cbk) {
 		try {
 			console.log("DEBUG Simplicite.UI.hooks.PmAssignment hook");
-			var p = o.locals.ui;
-			if (p && o.isMainInstance()) {
-				p.form.onload = function(ctn, obj, params) {
-					var extobj = "PmTimesheetRecapExt"; 
-					var embedded = true; 
-					var param = null; 
-					var url = app.getExternalObjectURL(extobj, param, embedded);
-					console.log("url = " + url);
-					$("#timesheet_PmTimeSheetAssign").after("<div id=recap></div>")
-					$ui.loadURL($('#recap'), url, { showNav:false });
-				};
-			}
+			app = $ui.getAjax(); 
+			var extobj = "PmTimesheetRecapExt"; 
+			var embedded = true; 
+			var param = null; 
+			var url = app.getExternalObjectURL(extobj, param, embedded);
+			console.log("url = " + url);
+			$ui.loadURL($('#recap'),url,null);
+			console.log("PmTimesheetExt");
 		} catch (e) {
 			app.error("Error in Simplicite.UI.hooks.PmAssignment: " + e.message);
 			console.log("Error in Simplicite.UI.hooks.PmAssignment: " + e.message);
