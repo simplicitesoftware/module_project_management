ba96cf79c5a891b9a9db8a30bbd4e0375c05448b
Candice Bimont <cbimont@simplicite.fr>
2023-04-19 17:30:25 +0200

    cra recap

--- resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
+++ resources/ObjectExternal/PmTimesheetExt/SCRIPT.js
@@ -4,14 +4,15 @@ var PmTimesheetExt = PmTimesheetExt || (function($) {
     function render(params) {
         var userid =$ui.grant.getUserID();
         $ui.displayTimesheet($('#ts'), "PmUser",userid , "PmTimeSheetAssign");
-        /* app = $ui.getAjax(); 
+        console.log("DEBUG in PmTimesheetExt");
+        app = $ui.getAjax(); 
 		var extobj = "PmTimesheetRecapExt"; 
 		var embedded = true; 
 		var param = null; 
 		var url = app.getExternalObjectURL(extobj, param, embedded);
 		console.log("url = " + url);
 		$ui.loadURL($('#recap'),url,null);
-        console.log("PmTimesheetExt"); */
+        console.log("PmTimesheetExt");
     }        
     return { render: render };
 })(jQuery);
