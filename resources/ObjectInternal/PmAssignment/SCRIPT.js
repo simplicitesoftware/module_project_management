3c677775f0c769c8afe56f60337f07816a8b5d88
Candice Bimont <cbimont@simplicite.fr>
2023-04-19 17:12:40 +0200

    cra recap

--- resources/ObjectInternal/PmAssignment/SCRIPT.js
+++ resources/ObjectInternal/PmAssignment/SCRIPT.js
@@ -14,6 +14,7 @@
 					var param = null; 
 					var url = app.getExternalObjectURL(extobj, param, embedded);
 					console.log("url = " + url);
+					$("#timesheet_PmTimeSheetAssign").after("<div id=recap></div>")
 					$ui.loadURL($('#recap'), url, { showNav:false });
 				};
 			}
