28355ad8622dba1d1774c0105e7532d7cddff2d3
Candice Bimont <cbimont@simplicite.fr>
2023-04-19 18:18:05 +0200

    cra recap

--- resources/ObjectExternal/PmTimesheetRecapExt/SCRIPT.js
+++ resources/ObjectExternal/PmTimesheetRecapExt/SCRIPT.js
@@ -43,7 +43,7 @@ var PmTimesheetRecapExt = PmTimesheetRecapExt || (function($) {
         if (list && list.length){
             data.listNotEmpty = true;
             list.forEach(ass => data.ass.push(function(assign){
-                console.log(typeof(assign));
+                console.log(Object.getOwnPropertyNames(assign));
                 var objAss = {
                 	pmAssTskName: assign.getField("pmTskTitle"),
                     pmAssRole: assign.pmAssRole,
