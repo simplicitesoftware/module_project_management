d643ed69b6bb6ff57521a36cd2f622127db9f722
Candice Bimont <cbimont@simplicite.fr>
2023-04-19 18:25:12 +0200

    cra recap

--- resources/ObjectInternal/PmAssignment/SCRIPT.js
+++ resources/ObjectInternal/PmAssignment/SCRIPT.js
@@ -3,23 +3,25 @@
 	if (!ui) return;
 	const app = ui.getAjax();
 	// Hook called by each object instance
+	console.log("DEBUG in PmAssignment.");
 	Simplicite.UI.hooks.PmAssignment = function(o, cbk) {
 		try {
-			console.log("DEBUG Simplicite.UI.hooks.PmAssignment hook");
-			var extobj = "PmTimesheetRecapExt"; 
-			var embedded = true; 
-			var param = null; 
-			var url = app.getExternalObjectURL(extobj, param, embedded);
-			console.log("url = " + url);
-			$ui.loadURL($('#recap'),url,null);
-			console.log("PmTimesheetExt");
+			var p = o.locals.ui;
+			if (p && o.isMainInstance()) {
+				p.form.onload = function(ctn, obj, params) {
+					var extobj = "PmTimesheetRecapExt"; 
+					var embedded = true; 
+					var param = null; 
+					var url = app.getExternalObjectURL(extobj, param, embedded);
+					console.log("url = " + url);
+					$ui.loadURL($('#recap'), url, { showNav:false });
+				};
+			}
 		} catch (e) {
 			app.error("Error in Simplicite.UI.hooks.PmAssignment: " + e.message);
-			console.log("Error in Simplicite.UI.hooks.PmAssignment: " + e.message);
 		} finally {
 			console.log("PmAssignment hooks loaded.");
 			cbk && cbk(); // final callback
 		}
 	};
 })(window.$ui);
- 
